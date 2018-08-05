c8683267bdb9559e3e7610e18d1016b76aec9d76
Tom <398374123@qq.com>
2018-08-05 17:28:59 +0800

    20180805
    practice

---
+++ JavaSE/JCF.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class JCF{
+	public static void main(String[] args) {	
+		//visit a list with iterator
+		ArrayList<String> list = new ArrayList<String>();
+		list.add(new String("Monday")); // push_back
+		list.add(new String("Tuesday"));
+		list.add(new String("Wensday"));
+		Iterator<String> it = list.iterator();
+		while(it.hasNext()){
+		    String weekday = it.next();
+		    System.out.println(weekday.toUpperCase());
+		}
+
+		list.remove(1);
+		for(String weekday : list){//enhanced for statement
+		    System.out.println(weekday);
+		}
+
+		list.set(1, "tbz");
+		System.out.println(list.get(1));
+
+
+		LinkedList<Integer> list2 = new LinkedList<Integer>();
+		list2.add(0, 1);
+		list2.add(2);
+		list2.add(1,6);
+		for (int x : list2){
+			System.out.println(x);
+		}
+	}
+}
