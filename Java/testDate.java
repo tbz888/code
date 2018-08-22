fe1a5d6543445a0a91ff0b57524ee2a4a2690d4c
Tom <tbz888@users.noreply.github.com>
2018-08-22 11:51:23 +0800

    20180822

---
+++ Java/testDate.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class testDate {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDate.of(1994,11,16));
+    }
+}
