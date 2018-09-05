87b25e3c26ca502e940c9e7c6ed0160567196959
Tom <398374123@qq.com>
2018-09-06 00:16:44 +0800

    20180906

--- Java/testThread.java
+++ Java/testThread.java
@@ -17,11 +17,40 @@ class Job implements Runnable {  // user's thread(No.2)
 }
 
 public class testThread {
+	protected static void testJoin() {
+
+		Runnable join1 = () -> {
+			for (int i=0; i<50; ++i) {
+				System.out.println(i);
+			}
+		};
+
+		Runnable join2 = () -> {
+			for (int i2=50; i2<100; ++i2) {
+				System.out.println(i2);
+			}
+		};
+
+		Thread t1 = new Thread(join1);
+		Thread t2 = new Thread(join2);
+
+		try {
+			t1.start();
+			t2.start();
+			t1.join(2); // get control for 2ms
+			t2.join(); // get control in t2
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {  // default thread(No.1)
 		System.out.println("Main_Begin: " + Thread.currentThread().toString() + Thread.currentThread().getState());
 		Thread r = new Thread(new Job()); // NEW
 		System.out.println(r.toString() + r.getState()); 
 		r.start(); // RUNNABLE
+		Thread.currentThread().yield(); // give up
 		System.out.println(r.toString() + r.getState()); 
 		System.out.println("Main_End: " + Thread.currentThread().toString() + Thread.currentThread().getState() + "\n");
 
@@ -40,5 +69,7 @@ public class testThread {
 				r.interrupt();
 			System.out.println("::Job_state: " + r.toString() + r.getState()); 
 		}
+
+		testJoin(); //serial
 	}
 }
