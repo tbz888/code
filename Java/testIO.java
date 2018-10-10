1cd8c1f60e8f5ced9bb965f28e6d3241929f77f0
Tom <398374123@qq.com>
2018-10-10 23:37:29 +0800

    20181010

--- Java/testIO.java
+++ Java/testIO.java
@@ -3,41 +3,63 @@ import java.util.zip.*;
 import java.io.Reader.*;
 
 public class testIO {
-
-	// private static void stdIO() {
-	// 	try {
-	// 		InputStreamReader in = new InputStreamReader(System.in);
-	// 		PrintWriter out = new PrintWriter(new FileWriter("test.txt"), true);
-	// 		out.print(in.read());
-	// 	}
-	// 	catch (IOException e) {
-	// 		e.printStackTrace();
-	// 	}
-	// }
-
-	// private static void fileIO() {
-	// 	try {
-	// 		InputStream fin = new FileInputStream("test.txt");
-	// 		byte b = (byte)fin.read();
-	// 		DataInputStream din = new DataInputStream(fin);
-	// 		double value = din.readDouble();
-
-	// 		ZipInputStream zin = new ZipInputStream(fin);
-
-	// 		InputStreamReader encode = new InputStreamReader(fin, "ISO8859_5");
-	// 	}
-	// 	catch (IOException e) {
-	// 		e.printStackTrace();
-	// 	}
-	// }
-
+	
+	// test data
     private static final int LEN = 5;
-    // abcdefghijklmnopqrsttuvwxyz
-    private static final byte[] ArrayLetters = {
+    private static final byte[] ArrayLetters = {   // abcdefghijklmnopqrsttuvwxyz
         0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
         0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A
     };
+
+
+    // File
+    private static void testFileInputOutputStream() throws IOException {
+		System.out.println("[testFileInputOutputStream]:");
+
+		FileOutputStream out = new FileOutputStream("PART3_IN_EPCC.log.1", true); //append
+		out.write("good bye.".getBytes());
+		out.close();
+
+		FileInputStream in = new FileInputStream("PART3_IN_EPCC.log.1");
+		byte[] buf = new byte[100];
+		in.read(buf);
+		System.out.println(new String(buf));
+	}
     
+    // Serializable and Externalizable object
+    private static void testObjectInputStreamOutputStream() throws IOException, ClassNotFoundException {
+    	System.out.println("[testObjectInputStreamOutputStream]:");
+
+    	FileOutputStream fout = new FileOutputStream("test.obj");
+    	ObjectOutputStream out = new ObjectOutputStream(fout);
+    	out.writeObject(String.valueOf("test"));
+    	out.writeObject(Double.valueOf(1.23));
+    	fout.close();
+    	out.close();
+
+		FileInputStream fin = new FileInputStream("test.obj");
+    	ObjectInputStream in = new ObjectInputStream(fin);
+    	System.out.println(in.readObject());
+    	System.out.println(in.readObject());
+    	fin.close();
+    	in.close();
+    }
+
+    // multithread message queue
+    private static void testPipedStream() throws IOException {
+    	System.out.println("[testPipedStream]:");
+    	PipedOutputStream out = new PipedOutputStream();
+    	PipedInputStream in = new PipedInputStream();
+    	out.connect(in);
+    	out.write(ArrayLetters);
+    	byte[] buf = new byte[26];
+    	in.read(buf);
+    	System.out.println(new String(buf));
+    	
+    	out.close();
+    	in.close();
+    }
+
     private static void testByteArrayInputStream() throws IOException {
     	System.out.println("[testByteArrayInputStream]:");
         ByteArrayInputStream bais = new ByteArrayInputStream(ArrayLetters);
@@ -69,6 +91,8 @@ public class testIO {
         else {
         	System.out.println("Not support mark.");
         }
+
+        bais.close();
     }
 
     private static void testByteArrayOutputStream() throws IOException {
@@ -80,15 +104,19 @@ public class testIO {
     	baos.write(0x43);
     	baos.write(0x64);        
         System.out.printf("%s\n", new String(baos.toByteArray()));
+
+        baos.close();
     }
 
 	public static void main(String[] args) {
 		try {
 			testByteArrayInputStream();
 			testByteArrayOutputStream();
-
+			testPipedStream();
+			testObjectInputStreamOutputStream();
+			testFileInputOutputStream();
 		}
-		catch (IOException e) {
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
